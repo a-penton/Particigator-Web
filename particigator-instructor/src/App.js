27310598f5365da1b80bf0276f4235014d6674c0
Andrew Penton <apenton@ufl.edu>
2023-03-23 16:01:54 -0400

    Add appropriate components, create state for if the user is logged in, and pass it to the components as needed.

--- particigator-instructor/src/App.js
+++ particigator-instructor/src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header'
-import Login from './components/Login'
+import React, {useState} from 'react'
+import Header from './components/Header';
+import Login from './components/Login';
+import Home from './components/Home';
+import Grades from './components/Grades';
+import Assignments from './components/Assignments';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <div className="App">
       <Header />
       <Router>
         <Routes>
-          <Route path="/" element={<Login />}></Route>
-          <Route path="/assignments" element={<Login />}></Route>
-          <Route path="/grades" element={<Login />}></Route>
+          <Route path="/" element={<Home loggedIn={loggedIn} />} />
+          <Route path="/login" element={<Login loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+          <Route path="/assignments" element={<Assignments loggedIn={loggedIn} />} />
+          <Route path="/grades" element={<Grades loggedIn={loggedIn} />} />
         </Routes>
       </Router>
     </div>
