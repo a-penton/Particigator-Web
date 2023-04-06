d800ef8f964e2e732d0dd7bac96eb528589510bb
robinfintz <peacerobin15@gmail.com>
2023-04-05 23:40:46 -0400

    Connect front end to retrieve all admin. Create working form to post new questions.

---
+++ particigator-instructor/src/components/AdminList.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import { API } from "../API";
+import "./UsersList.css"
+
+const fetchAdmin = async () => {
+  return await API.getAllAdmin();
+}
+
+const AdminList = () => {
+  const [admin, setAdmin] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchAdmin();
+        setAdmin(data);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  
+  return (
+    <div>
+    {isLoading ? <p>Loading...</p> : null}
+    {error ? <p>Error: {error.message}</p> : null}
+    <div className="Tabling">
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>ID</th>
+            <th>Students</th>
+          </tr>
+      {admin !== null ? 
+        admin.map((adminer) => {
+          return (
+            <tr>
+              <td>{adminer.username}</td>
+              <td>{adminer.id}</td>
+              <td>{adminer.students.map((student) => {
+                return (
+                    <div>
+                        <p>{student}</p>
+                    </div>
+                )
+              })}</td>
+            </tr>
+          )
+        }) : null}
+      </table>
+    </div>
+    </div>
+  );
+  }
+    
+    export default AdminList;
