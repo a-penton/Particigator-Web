6dba5c86f48840b16619bf370f4100b5ddfc9c23
Andrew Penton <apenton@ufl.edu>
2023-04-11 23:32:28 -0400

    Add primary home page content. The admin table still needs to be removed.

--- particigator-instructor/src/components/Home.js
+++ particigator-instructor/src/components/Home.js
@@ -1,21 +1,32 @@
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { LoginContext } from '../LoginContext';
 import NavBar from './NavBar';
 import AdminList from './AdminList';
+import './Home.css';
 
 function Home() {
 
 	const {loggedIn} = useContext(LoginContext);
+	const navigate = useNavigate();
 
 	if (!loggedIn) {
-		return <Navigate to="/login" />
+		navigate('/login');
 	}
+
+	// TODO: get actual user's name from database
+	const name = "Aman";
 
 	return (
 		<div>
 			<NavBar />
-			<h1>Home Page</h1>
+			<h1>Welcome, {name}!</h1>
+			<h4>What would you like to do today?</h4>
+			<div className="buttons">
+				<button onClick={() => {navigate('/assignments')}}>Assignments</button>
+				<button onClick={() => {navigate('/grades')}}>Grades</button>
+			</div>
+			<br />
 			<AdminList/>
 		</div>
 	)
