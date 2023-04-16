eb4f60dff1db916ac5caf1dc1981003ea0b7afbe
Andrew Penton <apenton@ufl.edu>
2023-04-16 13:10:03 -0400

    Fix logout button and add styling to current page in NavBar.

--- particigator-instructor/src/components/NavBar.js
+++ particigator-instructor/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useContext } from 'react';
 import { LoginContext } from '../LoginContext';
@@ -9,7 +9,23 @@ import "./NavBar.css";
 function NavBar() {
 
   const {loggedIn, setLoggedIn} = useContext(LoginContext);
+  const [current, setCurrent] = useState('');
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (window.location.pathname == '/') {
+      setCurrent('home');
+    }
+    else if (['/assignments', '/editAssignment', '/createAssignment'].includes(window.location.pathname)) {
+      setCurrent('assignments');
+    }
+    else if (window.location.pathname == '/grades') {
+      setCurrent('grades');
+    }
+    else if (window.location.pathname == '/students') {
+      setCurrent('students');
+    }
+	}, [])
 
   function logout() {
     setLoggedIn(false);
@@ -24,13 +40,13 @@ function NavBar() {
         <h3>Particigator</h3>
 			</div>
       <div className="center links">
-        <Link to="/">Home</Link>
-        <Link to="/assignments">Assignments</Link>
-        <Link to="/grades">Grades</Link>
-        <Link to="/students">Students</Link>
+        <Link className={(current === 'home' ? "current " : "") + "navlink"} to="/">Home</Link>
+        <Link className={(current === 'assignments' ? "current " : "") + "navlink"} to="/assignments">Assignments</Link>
+        <Link className={(current === 'grades' ? "current " : "") + "navlink"} to="/grades">Grades</Link>
+        <Link className={(current === 'students' ? "current " : "") + "navlink"} to="/students">Students</Link>
       </div>
       <div className="right">
-        <Link className="logout" onClick={logout}>Signout</Link>
+        <a className="logout" onClick={logout} href="/login">Signout</a>
       </div>
     </div>
   )
