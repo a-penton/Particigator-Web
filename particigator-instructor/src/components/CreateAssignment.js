e1fd0d8b2f7a612cc33f2c989cca17c054d203ed
Andrew Penton <apenton@ufl.edu>
2023-04-15 20:28:04 -0400

    Refactor assignment creation to work with database (renamed and new fields).

--- particigator-instructor/src/components/CreateAssignment.js
+++ particigator-instructor/src/components/CreateAssignment.js
@@ -13,16 +13,15 @@ function CreateAssignment() {
 	const navigate = useNavigate();
 	const {loggedIn} = useContext(LoginContext);
 
-	const [title, setQuestionTitle] = useState("");
+	const [questionTitle, setQuestionTitle] = useState("");
 	const [question, setQuestion] = useState("");
 	const [correctAnswer, setCorrectAnswer] = useState("");
 	const [incorrectAnswer1, setIncorrectAnswer1] = useState("");
 	const [incorrectAnswer2, setIncorrectAnswer2] = useState("");
 	const [incorrectAnswer3, setIncorrectAnswer3] = useState("");
-	const [formData, setFormData] = useState({
-		text: '',
-		title: '',
-	});
+	const [imagePath, setImagePath] = useState("14KeEfpHRFnQFoqb5zixvecFTk07zqT0Y");
+	const [explanation, setExplanation] = useState("");
+	const [passcode, setPasscode] = useState("");
 
 	const [questionSubmitted, setQuestionSubmitted] = useState(false)
 
@@ -38,24 +37,28 @@ function CreateAssignment() {
 	function submitAssignment(event) {
 		event.preventDefault();
 
-		postNewQuestion(formData);
+		postNewQuestion({
+			questionTitle: questionTitle,
+			question: question,
+			options: [
+				{id: "0", option: "A", answer: correctAnswer},
+				{id: "1", option: "B", answer: incorrectAnswer1},
+				{id: "2", option: "C", answer: incorrectAnswer2},
+				{id: "3", option: "D", answer: incorrectAnswer3}
+			],
+			correctAnswerIndex: 0,
+			imagePath: imagePath,
+			explanation: explanation,
+			passcode: passcode
+		});
 		setQuestionSubmitted(true);
 	}
 
 	function handleQuestionTitleChange(event) {
 		setQuestionTitle(event.target.value);
-		setFormData({
-			...formData,
-			title: event.target.value
-		});
-		
 	}
 	function handleQuestionChange(event) {
 		setQuestion(event.target.value);
-		setFormData({
-			...formData,
-			text: event.target.value
-		});
 	}
 	function handleCorrectAnswerChange(event) {
 		setCorrectAnswer(event.target.value);
@@ -69,6 +72,15 @@ function CreateAssignment() {
 	function handleIncorrectAnswer3Change(event) {
 		setIncorrectAnswer3(event.target.value);
 	}
+	function handleImagePathChange(event) {
+		setImagePath(event.target.value);
+	}
+	function handleExplanationChange(event) {
+		setExplanation(event.target.value);
+	}
+	function handlePasscodeChange(event) {
+		setPasscode(event.target.value);
+	}
 
 	return (
 		<div>
@@ -80,7 +92,7 @@ function CreateAssignment() {
 					<input 
 						type="text"
 						name="Title"
-						value={title}
+						value={questionTitle}
 						placeholder="Enter assignment title here!"
 						onChange={handleQuestionTitleChange}
 					/>
@@ -133,6 +145,36 @@ function CreateAssignment() {
 						value={incorrectAnswer3}
 						placeholder="Enter incorrect answer #3 here!"
 						onChange={handleIncorrectAnswer3Change}
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="Image Path">Image ID: </label>
+					<input
+						type="text"
+						name="Image Path"
+						value={imagePath}
+						placeholder="Enter image ID here!"
+						onChange={handleImagePathChange}
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="Explanation">Explanation: </label>
+					<input
+						type="text"
+						name="Explanation"
+						value={explanation}
+						placeholder="Enter explanation here!"
+						onChange={handleExplanationChange}
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="Passcode">Passcode: </label>
+					<input
+						type="text"
+						name="Passcode"
+						value={passcode}
+						placeholder="Enter passcode here!"
+						onChange={handlePasscodeChange}
 					/>
 				</div>
 				<button>Add this Assignment!</button>
